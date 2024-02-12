565793751625002a8cea1873e690985e7ff43b72
Yaroslav <dr.yasi@yandex.ru>
2024-02-12 15:50:02 +0300

    Add level system

--- project_bot.py
+++ project_bot.py
@@ -14,6 +14,12 @@ db = sqlite3.connect('level.db')
 sql = db.cursor()
 db.commit()
 
+# level_limit = dict[
+#     0: 0,
+#     1: 50,
+#     2: 150
+# ]
+
 # Создаем бота
 bot = commands.Bot(command_prefix="/", help_command=None, intents=disnake.Intents.all())
 
@@ -35,8 +41,33 @@ async def on_ready():
         print('Возникла ошибка во время запуска базы данных')
     finally:
         print(f"Bot {bot.user} is ready to work!")
+
+        for guild in bot.guilds:
+            for member in guild.members:
+                if member.bot:
+                    pass
+                else:
+                    if sql.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is None:
+                        sql.execute(f"INSERT INTO users VALUES ('{member}', {member.id}, 0, 1)")
+                    else:
+                        pass
+
         db.commit()
         print('client connected')
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    sql.execute("UPDATE users SET exp = exp + 10 WHERE id = {}".format(message.author.id))
+    db.commit()
+    cur_lvl = sql.execute("SELECT level FROM users WHERE id = {}".format(message.author.id)).fetchone()[0]
+    cur_exp = sql.execute("SELECT exp FROM users WHERE id = {}".format(message.author.id)).fetchone()[0]
+    if int((cur_exp - 50) / 100) + 1 > cur_lvl:
+        sql.execute("UPDATE users SET level = level + 1 WHERE id = {}".format(message.author.id))
+        sql.execute("UPDATE users SET exp = 0 WHERE id = {}".format(message.author.id))
+        await message.channel.send(f"Поздравляю, {message.author.mention}, ваш уровень повышен до {cur_lvl + 1}")
 
 
 # Игровая команда "Орел и решка"
