a11ab10915885dcc644123796cf455a0139ba1f7
Yaroslav <dr.yasi@yandex.ru>
2024-02-12 14:19:10 +0300

    Add new command - game 'Орел и решка'

--- project_bot.py
+++ project_bot.py
@@ -2,6 +2,7 @@
 import os
 import disnake
 from dotenv import load_dotenv
+from random import choice
 from disnake.ext import commands
 from disnake import ApplicationCommandInteraction
 
@@ -18,6 +19,48 @@ bot = commands.Bot(command_prefix="/", help_command=None, intents=disnake.Intent
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user} is ready to work!")
+
+
+# Игровая команда "Орел и решка"
+@bot.slash_command(description="Сыграть в монеточку")
+async def coin(ctx, side: str = commands.Param(choices=["орел", "решка"])):
+    sides = ['орел', 'решка']
+    botChoice = choice(sides)
+    if botChoice == side and side == 'орел':
+        embed = disnake.Embed(
+            title="Выпал орёл, вы выиграли",
+            description="Congratz!!!",
+            colour=0xF0C43F,
+        )
+        embed.set_image(url="https://i.ibb.co/hVrnL9F/image.png")
+        await ctx.send(embed=embed)
+
+    elif botChoice == side and side == 'решка':
+        embed = disnake.Embed(
+            title="Выпала решка, вы выиграли",
+            description="Congratz!!!",
+            colour=0xF0C43F,
+        )
+        embed.set_image(url="https://i.ibb.co/tZq8GQ5/image.png")
+        await ctx.send(embed=embed)
+
+    elif botChoice != side and side == 'решка':
+        embed = disnake.Embed(
+            title="Выпал орёл, вы проиграли",
+            description="unlucky",
+            colour=0xF0C43F,
+        )
+        embed.set_image(url="https://i.ibb.co/hVrnL9F/image.png")
+        await ctx.send(embed=embed)
+
+    elif botChoice != side and side == 'орел':
+        embed = disnake.Embed(
+            title="Выпала решка, вы проиграли",
+            description="unlucky",
+            colour=0xF0C43F,
+        )
+        embed.set_image(url="https://i.ibb.co/tZq8GQ5/image.png")
+        await ctx.send(embed=embed)
 
 
 # Запуск бота
