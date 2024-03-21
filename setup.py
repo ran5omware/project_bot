3138a49400bd336670eea5891a5072978648e8f5
ran5omware <dr.yasi04@gmail.com>
2024-03-21 23:27:37 +0300

    Add sphinx

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="TRPP_project",
     description="library for static functions",
